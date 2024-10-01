8f42b7f0544e6973f356532ae9acbceb842f549f
ZOZOliv <34007162+ZOZOliv@users.noreply.github.com>
2024-10-01 16:48:52 +0200

    Update communication.py

--- RaspberryPi/communication.py
+++ RaspberryPi/communication.py
@@ -1,26 +1,47 @@
-import serial, time
+import threading
+import queue
+import serial
+import time
 
-if __name__ == '__main__':
-    print('Running. Press CTRL-C to exit.')
+q_cmd = queue.Queue()
+stop_event = threading.Event()
+
+def com_arduino():
     #with serial.Serial(port="/dev/ttyACM0", baudrate=9600, timeout=1) as arduino: #Linux
     with serial.Serial(port="COM3", baudrate=9600, timeout=1) as arduino: #Windows
         time.sleep(0.1) #wait for serial to open
         if arduino.isOpen():
-            print("{} connected!".format(arduino.port))
-            try:
-                while True:
-                    cmd = input("Enter command (CMD:255,255,255,255): ")
+            print("{} connecté !".format(arduino.port))
+            while True:
+                if not q_cmd.empty():
+                    cmd = q_cmd.get()
                     arduino.write(cmd.encode())
-                    #time.sleep(0.1) #wait for arduino to answer
-                    
-                    while arduino.inWaiting() == 0: 
-                        pass
-                    
-                    if arduino.inWaiting() > 0: 
-                        answer = str(arduino.readline())
-                        print("---> {}".format(answer))                            
-                        arduino.flushInput() #remove data after reading
-                            
-            except KeyboardInterrupt:
-                arduino.close()
-                print("KeyboardInterrupt has been caught.")
+                    while arduino.out_waiting > 0: 
+                        time.sleep(0.2)
+
+                
+                while arduino.in_waiting > 0: 
+                    answer = str(arduino.readline())
+                    print("---> {}".format(answer))                            
+                    arduino.flushInput() #remove data after reading
+
+                if stop_event.is_set():
+                    arduino.close()
+                    print("{} déconnecté...".format(arduino.port))
+                    break
+
+
+thread = threading.Thread(target=com_arduino, daemon=True)
+thread.start()
+
+try:
+    while True:
+        cmd = input("Enter command (CMD:255,255,255,255): ")
+        #cmd = "CMD:255,255,255,255"
+        q_cmd.put(cmd)
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    stop_event.set()
+    thread.join()
+    print("Arrêt du programme")
