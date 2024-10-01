88bb37b53670356b17145c306483c8bb00cab4c3
ZOZOliv <34007162+ZOZOliv@users.noreply.github.com>
2024-10-01 14:21:57 +0200

    Update communication.py

--- RaspberryPi/communication.py
+++ RaspberryPi/communication.py
@@ -1,20 +1,15 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# lsusb to check device name
-#dmesg | grep "tty" to find port name
-
-import serial,time
-
+import serial, time
 
 if __name__ == '__main__':
     print('Running. Press CTRL-C to exit.')
-    with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
+    #with serial.Serial(port="/dev/ttyACM0", baudrate=9600, timeout=1) as arduino: #Linux
+    with serial.Serial(port="COM3", baudrate=9600, timeout=1) as arduino: #Windows
         time.sleep(0.1) #wait for serial to open
         if arduino.isOpen():
             print("{} connected!".format(arduino.port))
             try:
                 while True:
-                    cmd = input("Enter command (DATA, CMD_D, CMD_G, CMD_H, CMD_B): ")
+                    cmd = input("Enter command (CMD:255,255,255,255): ")
                     arduino.write(cmd.encode())
                     #time.sleep(0.1) #wait for arduino to answer
                     
@@ -23,14 +18,9 @@ if __name__ == '__main__':
                     
                     if arduino.inWaiting() > 0: 
                         answer = str(arduino.readline())
-                        print("---> {}".format(answer))
-                        if cmd == "data":
-                            dataList = answer.split("x")
-                            print("led state : {}".format(dataList[0]))
-                            print("Analog input A0 : {}".format(dataList[1]))
-                            print("Analog input A1: {}".format(dataList[2]))
-                            
-                            arduino.flushInput() #remove data after reading
+                        print("---> {}".format(answer))                            
+                        arduino.flushInput() #remove data after reading
                             
             except KeyboardInterrupt:
+                arduino.close()
                 print("KeyboardInterrupt has been caught.")
